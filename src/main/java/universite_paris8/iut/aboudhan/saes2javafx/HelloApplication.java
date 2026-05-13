5183d6ed2b5c406f2b4e026097264d5005d17ebb
Aymane BOUDHAN <aboudhan@iut.univ-paris8.fr>
2026-05-13 16:18:38 +0200

    commit initial

---
+++ src/main/java/universite_paris8/iut/aboudhan/saes2javafx/HelloApplication.java
@@ -0,0 +1,23 @@
+package universite_paris8.iut.aboudhan.saes2javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("vueSAE-S2.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1165, 785);
+        stage.setTitle("InterfaceJeu");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
