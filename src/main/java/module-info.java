a929a1d32c1a2b024b7875df1b2c5f8a49bb48b5
Aymane <aymane.boudhan920@gmail.com>
2026-05-25 00:37:41 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module universite_paris8.iut.aboudhan.saes2javafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens universite_paris8.iut.aboudhan.saes2javafx to javafx.fxml;
-    exports universite_paris8.iut.aboudhan.saes2javafx;
-}
