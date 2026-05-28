baf3c5373037406c6a8407ba5368be9c27dcad06
Aymane <aymane.boudhan920@gmail.com>
2026-05-28 15:01:30 +0200

    Update MicrobeVue.java

--- src/main/java/universite_paris8/iut/aboudhan/saes2javafx/vue/MicrobeVue.java
+++ src/main/java/universite_paris8/iut/aboudhan/saes2javafx/vue/MicrobeVue.java
@@ -6,25 +6,28 @@ import universite_paris8.iut.aboudhan.saes2javafx.modele.Microbe;
 
 public class MicrobeVue extends ImageView {
 
-    private Microbe modele;
+    private Microbe microbe;
     private ProgressBar barreDeVie;
     private VBox conteneurGraphique;
 
-    public MicrobeVue(Microbe modele) {
-        this.modele = modele;
+    public MicrobeVue(Microbe m) {
+        this.modele = m;
 
         //Pour barre de vie
-        this.barreDeVie = new ProgressBar(modele.getRatioPV());
+        this.barreDeVie = new ProgressBar(microbe.getRatioPV());
         this.barreDeVie.setPrefWidth(25);
         this.barreDeVie.setPrefHeight(3);
         this.barreDeVie.getStyleClass().add("barre-vie-microbe");
 
         //Pour afficher l'image du microbe
-        ImageView imageMicrobe = new ImageView(new Image(getClass().getResourceAsStream(modele.getNomImage())));
+        ImageView imageMicrobe = new ImageView(new Image(getClass().getResourceAsStream(microbe.getNomImage())));
         imageMicrobe.setFitWidth(34);
         imageMicrobe.setFitHeight(34);
         imageMicrobe.setPreserveRatio(true);
 
+        // Positionnement initial
+        mettreAJourPosition();
+        
         //Rassembler la barre de vie avec le microbe
         this.conteneurGraphique = new VBox(4);
         this.conteneurGraphique.setAlignment(Pos.CENTER);
@@ -35,12 +38,9 @@ public class MicrobeVue extends ImageView {
 
     }
 
-    public void mettreAJour() {
-
-        // On retire 8 pixels en Y pour que la barre de vie soit au-dessus sans décaler le microbe
-        this.conteneurGraphique.setLayoutX(modele.getX());
-        this.conteneurGraphique.setLayoutY(modele.getY() - 8);
-
+    public void mettreAJourPosition() {
+        this.setTranslateX(this.microbe.getX());
+        this.setTranslateY(this.microbe.getY());
         // Met à jour le remplissage de la jauge (entre 0.0 et 1.0)
         this.barreDeVie.setProgress(modele.getRatioPV());
     }
