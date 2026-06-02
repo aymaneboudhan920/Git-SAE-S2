fdf16f25034d6d0996a60a6c9e7431e919c5f79d
Nathan_Dq <nat.dequier@gmail.com>
2026-06-02 15:45:47 +0200

    Refactor Tour class and add creation methods
    Renamed parameter 'v' to 'vitesse' for clarity and added static methods to create specific Tour instances.

--- src/main/java/universite_paris8/iut/aboudhan/saes2javafx/modele/Tour.java
+++ src/main/java/universite_paris8/iut/aboudhan/saes2javafx/modele/Tour.java
@@ -4,10 +4,10 @@ public class Tour {
     private double x, y;
     private int portee;
     private int degats;
-    private String nomImage;
     private double v;
+    private String nomImage;
 
-    public Tour(double x, double y, int portee, int degats, double v, String nomImage) {
+    public Tour(double x, double y, int portee, int degats, double vitesse, String nomImage) {
         this.x = x;
         this.y = y;
         this.portee = portee;
@@ -16,6 +16,14 @@ public class Tour {
         this.nomImage = nomImage;
     }
 
+
+    public static Tour creerScientifique(double x, double y) {
+        return new Tour(x, y, 120, 15, 1.2, "/universite_paris8/iut/aboudhan/saes2javafx/vue/tour_scientifique.png");
+    }
+
+    public static Tour creerChimiste(double x, double y) {
+        return new Tour(x, y, 90, 25, 0.8, "/universite_paris8/iut/aboudhan/saes2javafx/vue/tour_chimiste.png");
+    }
     public double getX() { return x; }
     public double getY() { return y; }
     public int getPortee() { return portee; }
