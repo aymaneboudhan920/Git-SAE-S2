833be36152f7fc77a188210a45ab3ea590eb0af5
Aymane <aymane.boudhan920@gmail.com>
2026-06-11 00:43:06 +0200

    Ajout de la méthode d'attaque du chimiste

--- src/main/java/universite_paris8/iut/aboudhan/saes2javafx/modele/TourChimiste.java
+++ src/main/java/universite_paris8/iut/aboudhan/saes2javafx/modele/TourChimiste.java
@@ -13,6 +13,25 @@ public class TourChimiste extends Tour {
 
     @Override
     public void attaquer(List<Microbe> microbesActifs) {
-        // Todo : A faire plus tard pour le sprint 3
+        // On vérifie d'abord si le temps de recharge est écoulé
+        if (peutAttaquer()) {
+
+            // On parcourt tous les microbes actifs pour l'attaque de zone
+            for (int i = 0; i < microbesActifs.size(); i++) {
+                Microbe m = microbesActifs.get(i);
+
+                // On ne touche que les microbes vivants
+                if (!m.estMort()) {
+                    double diffX = m.getX() - this.getX();
+                    double diffY = m.getY() - this.getY();
+                    double distance = Math.sqrt(diffX * diffX + diffY * diffY);
+
+                    // Tous les microbes présents dans le rayon d'action subissent les dégâts
+                    if (distance <= this.getPortee()) {
+                        m.perdreVie(this.getDegats());
+                    }
+                }
+            }
+        }
     }
 }
