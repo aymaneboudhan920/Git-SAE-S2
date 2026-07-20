a4cf1bd3aadd1228b8f27451899048ef0d359c9d
Nathan_Dq <nat.dequier@gmail.com>
2026-07-20 20:52:44 +0200

    Refactor PotionSoin constants for clarity

--- src/main/java/universite_paris8/iut/aboudhan/saes2javafx/modele/potion/PotionSoin.java
+++ src/main/java/universite_paris8/iut/aboudhan/saes2javafx/modele/potion/PotionSoin.java
@@ -3,8 +3,8 @@ package universite_paris8.iut.aboudhan.saes2javafx.modele.potion;
 import universite_paris8.iut.aboudhan.saes2javafx.modele.jeu.Environnement;
 
 public class PotionSoin extends Potion {
-    public static int prixAchat = 150;
-    private final int nbSoignee = 10;
+    public static final int prixAchat = 150;
+    private static final int NB_SOIGNEE = 10;
 
     public PotionSoin() {
         super("Potion de Soin", prixAchat);
@@ -13,6 +13,6 @@ public class PotionSoin extends Potion {
     @Override
     public void appliquerEffet(Environnement env) {
         int infectesActuels = env.getGensInfectes();
-        env.setGensInfectes(Math.max(0, infectesActuels - nbSoignee));
+        env.setGensInfectes(Math.max(0, infectesActuels - NB_SOIGNEE));
     }
 }
